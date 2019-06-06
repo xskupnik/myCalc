8f363c89dce8d66df83abfdfa930a0e797843af5
xskupnik <renata.dohnalova@gmail.com>
2019-06-06 22:12:05 +0200

    test change

--- src/com/dohnalovar/Main.java
+++ src/com/dohnalovar/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-
+        //write the code here
         INode test1 = new Leaf(2);
         INode test2 = new Leaf(-1);
         AbsolutValue test3 = new AbsolutValue(test2);
